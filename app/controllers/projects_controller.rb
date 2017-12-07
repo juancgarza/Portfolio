2f77597e9aeadb24f925be9e480436fa214c70be
Juan Carlos Garza <jcgarzar@gmail.com>
2017-12-07 13:44:39 -0600

    Implemented edit and update funct

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,35 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(params.require(:project).permit(:title,:subtitle,:body))
+
+    respond_to do |format|
+      if @project.save
+        format.html {redirect_to projects_path, notice: "Your portfolio item is now live"}
+      else
+        format.html {render :new}
+      end
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    respond_to do |format|
+      if @project.update(params.require(:project).permit(:title,:subtitle,:body))
+        format.html { redirect_to projects_path, notice: 'Project was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
 end
