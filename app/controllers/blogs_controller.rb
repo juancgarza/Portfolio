683fc74f8965b7840c2bbc9a5e91f1359ce4842c
Juan Carlos Garza <jcgarzar@gmail.com>
2017-12-12 13:46:25 -0600

    updated data validations

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -66,6 +66,7 @@ class BlogsController < ApplicationController
     end
     redirect_to blogs_url , notice:"Post status was changed to #{@blog.status}"
   end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
