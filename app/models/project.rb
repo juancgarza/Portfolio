80c69df4294a87d109f9f4cc10303c6bb3f6cd54
Juan Carlos Garza <jcgarzar@gmail.com>
2017-12-12 20:40:03 -0600

    Added concern for image generator

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   validates_presence_of :title,:body,:main_image,:thumb_image
-
+  include Placeholder
   def self.angular
     where(subtitle:"Angular")
   end
@@ -9,7 +9,7 @@ class Project < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= "http://via.placeholder.com/600x400"
-    self.thumb_image ||= "http://via.placeholder.com/100x100"
+    self.main_image ||= Placeholder.image_generator(height:'600', width:'400')
+    self.thumb_image ||= Placeholder.image_generator(height:'600',width:'400')
   end
 end
