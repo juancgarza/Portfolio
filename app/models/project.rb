33fde8d65b551d8507ccdae3bbb33b31657b744c
Juan Carlos Garza <jcgarzar@gmail.com>
2017-12-12 20:28:30 -0600

    Added defaults to project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,15 @@
 class Project < ApplicationRecord
   validates_presence_of :title,:body,:main_image,:thumb_image
 
+  def self.angular
+    where(subtitle:"Angular")
+  end
+  scope :ruby_on_rails_project_items, -> {where(subtitle:"Ruby On Rails")}
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/100x100"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
+3.times do |topic|
+  Topic.create!(
+    title:"Topic #{topic}"
+  )
+end
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body:"Nulla est mi, lacinia in nibh sit amet, venenatis rhoncus nulla. Donec ullamcorper leo at diam congue, eget congue mauris fringilla. Nunc at egestas quam. Nulla facilisi. Aliquam feugiat ut nisl vel pulvinar. Cras lobortis magna vel sapien commodo, mattis varius lorem lobortis. Fusce vitae fermentum mauris, sit amet bibendum quam. In egestas tellus nec augue dapibus, in posuere nibh faucibus. Vivamus ut posuere sem. Mauris luctus gravida risus vel tincidunt. Donec non mauris ultricies, convallis dui sed, molestie sem."
+    body:"Nulla est mi, lacinia in nibh sit amet, venenatis rhoncus nulla. Donec ullamcorper leo at diam congue, eget congue mauris fringilla. Nunc at egestas quam. Nulla facilisi. Aliquam feugiat ut nisl vel pulvinar. Cras lobortis magna vel sapien commodo, mattis varius lorem lobortis. Fusce vitae fermentum mauris, sit amet bibendum quam. In egestas tellus nec augue dapibus, in posuere nibh faucibus. Vivamus ut posuere sem. Mauris luctus gravida risus vel tincidunt. Donec non mauris ultricies, convallis dui sed, molestie sem.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -19,7 +25,16 @@ puts "5 skills created"
 9.times do |project|
   Project.create!(
     title:"My Project #{project}",
-    subtitle:"Great service",
+    subtitle:"Ruby On Rails",
+    body:"In at ligula molestie nisl mattis iaculis. Duis porttitor, est nec facilisis auctor, ante turpis dictum magna, sed feugiat eros nibh sed mi. Etiam orci diam, maximus vel quam in, pellentesque tincidunt tellus. Donec id ante porttitor, volutpat lacus at, lobortis ipsum. Aliquam volutpat scelerisque lorem eu scelerisque. Etiam sed convallis odio. Nullam hendrerit at lectus nec porttitor. Donec consectetur faucibus mi, sed suscipit massa dapibus eu. Cras laoreet fringilla augue, vitae tempor orci commodo sed.",
+    main_image:"http://via.placeholder.com/600x400" ,
+    thumb_image:"http://via.placeholder.com/100x100"
+  )
+end
+1.times do |project|
+  Project.create!(
+    title:"My Project #{project}",
+    subtitle:"Angular",
     body:"In at ligula molestie nisl mattis iaculis. Duis porttitor, est nec facilisis auctor, ante turpis dictum magna, sed feugiat eros nibh sed mi. Etiam orci diam, maximus vel quam in, pellentesque tincidunt tellus. Donec id ante porttitor, volutpat lacus at, lobortis ipsum. Aliquam volutpat scelerisque lorem eu scelerisque. Etiam sed convallis odio. Nullam hendrerit at lectus nec porttitor. Donec consectetur faucibus mi, sed suscipit massa dapibus eu. Cras laoreet fringilla augue, vitae tempor orci commodo sed.",
     main_image:"http://via.placeholder.com/600x400" ,
     thumb_image:"http://via.placeholder.com/100x100"
